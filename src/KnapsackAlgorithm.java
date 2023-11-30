5b4a43fca03479e5701fab36399c91a0c06998b2
ElisabetAronsson <elisabet.aronsson24@gmail.com>
2023-11-30 20:33:49 +0100

    Lite labbförberedelser

--- src/KnapsackAlgorithm.java
+++ src/KnapsackAlgorithm.java
@@ -1,4 +1,36 @@
-import java.util.ArrayList;
+import java.io.*;
 
 public class KnapsackAlgorithm {
+
+    public KnapsackAlgorithm() throws IOException {
+        String filePath = "C:/Users/elisa/OneDrive/Dokument/GitHub/lab2_knapsack/src/InputFiles/input1";
+        //String filePath = "C:/Users/elisa/OneDrive/Dokument/GitHub/lab2_knapsack/src/InputFiles/input2";
+        //String filePath = "C:/Users/elisa/OneDrive/Dokument/GitHub/lab2_knapsack/src/InputFiles/input3";
+
+        BufferedReader reader = new BufferedReader((new FileReader(filePath)));
+        int maxWeight = Integer.parseInt(reader.readLine());
+        int n = Integer.parseInt(reader.readLine());
+
+        System.out.println("Max weight of knapsack: " + maxWeight);
+        for(int i = 0; i < n ; i++){
+            String[] line = reader.readLine().split("\\s+");
+            int value = Integer.parseInt(line[0]);
+            int weight = Integer.parseInt(line[1]);
+
+            System.out.println("Item " + (i + 1) + ": Value = " + value + ", Weight = " + weight);
+        }
+
+    }
+
+    public void greedyApproach(){
+
+    }
+
+    public void neighboursearchApproach(){
+
+    }
+
+    public void neighboursearchTaboApproach(){
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        //KnapsackAlgorithm algorithm = new Greedy();
-        //KnapsackAlgorithm algorithm = new NeighbourhoodSearch();
+    public static void main(String[] args) throws IOException {
+        new KnapsackAlgorithm();
     }
 }
